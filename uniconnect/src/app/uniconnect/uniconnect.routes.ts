02c564c7b7fe50e996f25840fe91c3502ab1e742
Jenciso20 <jencisoquispe@gmail.com>
2024-12-16 10:19:16 -0500

    ruta de user-profile agregado

--- uniconnect/src/app/uniconnect/uniconnect.routes.ts
+++ uniconnect/src/app/uniconnect/uniconnect.routes.ts
@@ -4,5 +4,13 @@ export const uniconnectRoutes: Routes = [
   {
     path: 'uniconnect',
     loadComponent: () => import('./uniconnect-list/uniconnect-list.component').then(m => m.UniconnectListComponent),
+  },
+  {
+    path: 'profile',
+    loadComponent: () => import('./profile/profile.component').then(m => m.ProfileComponent),
+  },
+  {
+    path: 'user-profile',
+    loadComponent: () => import('./user-profile/user-profile.component').then(m => m.UserProfileComponent),
   }
 ]
